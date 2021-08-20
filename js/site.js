43b4fa9f2a21eed12b22ae93ab34ed8d6343993b
Charles Ramcharan <charlesramcharan@yahoo.com>
2021-08-19 21:25:33 -0400

    start site.js file

--- js/site.js
+++ js/site.js
@@ -1,25 +1,47 @@
 //get values from the interface
 function getValues(){
-    //get values from the page
-        let startValue = document.getElementById('startValue').value;
-        let endValue = document.getElementById('endValue').value;
-        alert("the start value: " + startValue);
 
+    //get values from the page
+        let amount = document.getElementById('amount').value;
+        let term = document.getElementById('term').value;
+        let rate = document.getElementById('rate').value;
+        
+        let currentBalance = amount;
     //call generate numbers
 
-
+    let payment = calculateMonthlyPayment(amount, term, rate);
+    let interest = calculateMonthlyInterestPayment(currentBalance, rate);
+    let principal = payment - interest;
     //call display numbers
 
 }
-//gererate numbers from start value to end value
-function generateNumbers(start, end){
-
-    for(let i=start; i<=end; i++){
+function createData(month,term,amount, payment,interest){
+    
+    let lsObject = {};
+    let balance = 0;
+    let totalInterest
         
-    }
+            lsObject["month"] = month;
+            lsObject["payment"] = payment;
+            lsObject["principal"] = payment - interest;
+            lsObject["interest"] = interest;
+            lsObject["totalInterest"] = totalInterest;
+            lsObject["balance"] = balance;
+            lsArray.push(lsObject);
+        
+    return lsObject;
 }
 
-//display numbers and mark even numbers bold
-function displayNumbers(){
+
+function calculateMonthlyPayment(amount, term, rate){
+
+    let monthlyPayment = (amount)*(rate/1200)/(1-(1+rate/1200)**-term);
+
+    return monthlyPayment;
+
+}
+function calculateMonthlyInterestPayment(currentBalance, rate){
+ let interestPayment = currentBalance * (rate/1200);.3
+ return interestPayment;
 
 }
